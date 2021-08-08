98b60b5387e5aa4392c5f765035e9553198039b4
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-08-08 14:35:49 +0200

    MediatR Example

---
+++ MediatRDemo/Controllers/ContactsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("[controller]")]
+public class ContactsController : ControllerBase
+{
+    private IMediator mediator;
+    public ContactsController(IMediator mediator) => this.mediator = mediator;
+
+
+    [HttpGet("{Id}")]
+    public async Task<Contact> GetContact([FromRoute] Query query) => await mediator.Send(query);
+
+    #region Nested Classes
+    public class Query : IRequest<Contact>
+    {
+        public int Id { get; set; }
+    }
+
+    public class ContactHandler : IRequestHandler<Query, Contact>
+    {
+        private ContactsContext db;
+
+        public ContactHandler(ContactsContext db) => this.db = db;
+
+        public Task<Contact> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return db.Contacts.Where(c => c.Id == request.Id).SingleOrDefaultAsync();
+        }
+    }
+    #endregion
+}
---
+++ MediatRDemo/Startup.cs
@@ -0,0 +1,63 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace MediatRDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddMediatR(typeof(Startup));
+            services.AddDbContext<ContactsContext>(options => options.UseInMemoryDatabase(databaseName: "ContactsDB"));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MediatRDemo", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MediatRDemo v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ContactsContext>();
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
